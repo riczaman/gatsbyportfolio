19df3ac01ea474efa6e5e0a29e27e4430b11d540
Ricky Zaman <ricky.zaman@outlook.com>
2020-07-18 15:22:22 -0400

    buliding blocks

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi, Ricky!</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
